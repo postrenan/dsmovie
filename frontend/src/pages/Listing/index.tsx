2c37d84a7a2fbae7a8b34206dec4a77cd7d7b3d3
renan <renannikodem09@gmail.com>
2022-03-13 23:04:28 -0300

    Props 2

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -11,19 +11,19 @@ function Listing() {
     const [pageNumber, setPageNumber] = useState(0);
 
     const [page, setPage] = useState<MoviePage>({
-        content: [];
-        last: true;
-        totalPages: 0;
-        totalElements: 0;
-        size: 12;
-        number: 0;
-        first: true;
-        numberOfElements: 0;
-        empty: true;
+        content: [],
+        last: true,
+        totalPages: 0,
+        totalElements: 0,
+        size: 12,
+        number: 0,
+        first: true,
+        numberOfElements: 0,
+        empty: true
     });
 
     useEffect(() => {
-        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}`)
+        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=id`)
             .then(response => {
                 const data = response.data as MoviePage;
                 setPage(data);
