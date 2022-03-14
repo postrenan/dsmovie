84573e8d314fa80c3cbc12617088860b65cec312
renan <renannikodem09@gmail.com>
2022-03-13 22:21:26 -0300

    First request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,16 +1,23 @@
+import axios from 'axios';
 import MovieCard from 'components/MovieCard';
 import Pagination from 'components/Pagination';
 
 function Listing() {
+
+
+    //FORMA ERRADA
+    axios.get('http://localhost:8080/movies?size=12&page=0')
+        .then(response => {
+            console.log(response.data);
+        })
+
+
     return (
         <>
             <Pagination />
 
             <div className="container">
                 <div className="row">
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div>
                     <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
                         <MovieCard />
                     </div>
